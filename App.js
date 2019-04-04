d01135baf58d31639ad751764b03beca9f903f1e
AJ Arriola <angelojames.arriola@gmail.com>
2019-04-04 15:40:00 -0700

    added lineargradient styling and scrollview to app.js

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, FlatList } from 'react-native';
+import { LinearGradient } from 'expo';
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <LinearGradient 
+          colors={['#3f6b6b', '#121212']}
+          style={styles.header}
+        />
+        <FlatList style={styles.list} />
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  header: {
+    width: '100%',
+    height: 500
+  },
+  list: {
+    width: '100%',
+    height: 800,
+    backgroundColor: '#121212'
+  }
 });
+
+export default App;
